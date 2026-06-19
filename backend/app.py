b0580756215e23ae1a66a6d64c411b3c6814968c
Billy Eskein <billyeskein@gmail.com>
2026-06-19 12:32:46 +0300

    Initial project setup

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from extensions import db,migrate
+from config import Config
+
+app=Flask(__name__)
+
+app.config.from_object(Config)
+
+db.init_app(app)
+migrate.init_app(app,db)
